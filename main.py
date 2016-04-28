d192284350c00fd1f3b33f36472e3170555e69e0
Kristina <kriswagner.ch@gmail.com>
2016-04-27 22:57:11 -0400

    stats section fully functional

--- main.py
+++ main.py
@@ -87,7 +87,45 @@ def images():
     print images 
     return jsonify(images=images)
 
-
+@app.route('/featuresaverages')
+def featuresaverages():
+    print("starting to get pageviews!")
+    #In this list, we'll store pageviews, revisions and more 
+    allfeatures = [] 
+    #Here we'll store the averages. This is what we really care about tbh 
+    featuresaverages = [] 
+    #Get the list of similar subjects to the keyword searched 
+    key = str(request.args.get('key'))
+    anchors = []
+    url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="+key+"&srwhat=text&srprop=timestamp&continue"
+    jsondata = urllib2.urlopen(url).read()
+    data = json.loads(jsondata)
+    topics = [] 
+    #This is just a convolluted way or getting the value from within the JSON file. It is specific to this instance. 
+    for item in data:
+        val = data[item]
+        if (item == "query"):
+            for item2 in val:
+                val2 = val[item2]
+                if (item2 == "search"):
+                    for item3 in val2:
+                        p = (item3['title']).encode("utf-8")
+                        topics.append(p)
+    print("starting pageviews")
+    for item in topics: 
+        #Get the pageview for each subject in the list of similar subjects
+        pageviews_total = 0
+        pageviewsjson = requests.get("https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/"+item+"/daily/20150701/20160410").json()
+        print pageviewsjson.keys()
+        try:
+            pageviews = pageviewsjson['items']
+        except KeyError:
+            pageviews_total = "Not Available"
+        else:
+            for j in range(len(pageviews)):
+                pageviews_total = pageviews_total+pageviews[j]['views']
+        pageviews_average = pageviews_total/len(topics)*1.0 
+    return jsonify(featuresaverages = pageviews_total)
 
 @app.errorhandler(404)
 def page_not_found(e):
